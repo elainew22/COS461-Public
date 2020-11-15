3eec119513ec63c2aef54780350f599d216a6899
Jana Sebaali <jsebaali@princeton.edu>
2020-11-15 14:29:59 -0500

    debugging

--- assignments/assignment5/http_proxy.go
+++ assignments/assignment5/http_proxy.go
@@ -10,17 +10,16 @@
 
  // Importing packages 
  import (
-   "fmt"
    "io"
    "os"
    "log"
    "net"
    "bufio"
    "bytes"
-   "strings"
    "net/http"
-   "net/url"
    "io/ioutil"
+   "strings"
+   "fmt"
  )
  
  const SEND_BUFFER_SIZE = 2048
@@ -35,42 +34,50 @@
 	c.Write(b)
  }
  
- func checkError(err error, c net.Conn){
+ func checkError(err error, c net.Conn) (int){
 	 if err != nil {
 		  b := []byte("400 Bad Request Error")
 		  c.Write(b)
+          return -1
 	 }
+     return 0
  }
  
  
  func handleConnection(c net.Conn){
+    fmt.Println("in handle")
  
-	 var data [SEND_BUFFER_SIZE]byte
-	 var builder strings.Builder
+     data := make([]byte, SEND_BUFFER_SIZE)
+     var buffer bytes.Buffer
 	 var err error 
  
 	 // read request in newReader
-
-
 	 for err != io.EOF {
 	   _ , err := c.Read(data)
-	   checkError(err, c)
+	   if checkError(err, c) == -1 {return}
 	   
-	   lengthData, errW := builder.write(data)
-	   checkError(errW, c) // TODO need to return from method if error
+	   _, errW := buffer.Write(data)
+      if checkError(errW, c) == -1 {return}
+      fmt.Println("here")
 	 }
  
 	 // read request from reader
-	 reader := bufio.NewReader(builder.String())
-	 req, err := http.ReadRequest(reader)
+	 var reader io.Reader
+     reader.Read(buffer.Bytes())
+     readerBuf := bufio.NewReader(reader)
+     
+	 req, err := http.ReadRequest(readerBuf)
+     if checkError(err, c) == -1 {return}
  
 	 if req.Method != "GET" {
 		badReqError(c)
+        return
 	 } else {
 	 
 	 // check if it is HTTP 1.1
 		 if req.Proto != "HTTP/1.1" {
 		   internalError(c)
+           return
 		 }
 		 
 		 // update the header to include close connection
@@ -83,63 +90,67 @@
 		
 		 // get the host url
 		 host := req.URL.Host
-		 u, err := url.Parse(host)
-		 checkError(err, c)
-		 
-		 // if the port is not available, append the default port
-		 port := u.Port()
-		 if port==""{
-		  host = host + ":80"
-		  port = "80"
-		 }
+         if strings.LastIndex(host, ":") == -1 {
+          host = host + ":80"
+        }
+       
 		 
 		 // connect to server
 		 cServer, err := net.Dial("tcp", host)
-		 checkError(err, c)
+         if checkError(err, c) == -1 {return}
 		 
 		 // send the request to the server
 		 body, err := ioutil.ReadAll(req.Body)
-		 cServer.Write(string(body))
+         if checkError(err, c) == -1 {return}
+		 cServer.Write(body)
 		 
 		 // read the response from the connection
-		 var dataRes [SEND_BUFFER_SIZE]byte
-		 var builderRes strings.builder
-		 // is err getting reset here?
-		 //err := nil
+         dataRes := make([]byte, SEND_BUFFER_SIZE)
+		 var bufferRes bytes.Buffer
+         
 		 err = nil
 		 for err != io.EOF {
 		   _ , err := c.Read(dataRes)
-		   checkError(err, c)
+		   if checkError(err, c) == -1 {return}
 		   
-		   _ , errW := builderRes.write(dataRes)
-		   checkError(errW, c)
+		   _ , errW := bufferRes.Write(dataRes)
+	       if checkError(errW, c) == -1 {return}
+        fmt.Println("here")
 		 }
  
 		 // read response from reader
-		 readerRes := bufio.NewReader(builderRes.String())
+        var readerR io.Reader
+        readerR.Read(bufferRes.Bytes())
+        readerRBuf := bufio.NewReader(readerR)
+	
 		 
 		 // send the response to the client
 		 buf := new(bytes.Buffer)
-		 buf.ReadFrom(readerRes)
+		 buf.ReadFrom(readerRBuf)
 		 c.Write(buf.Bytes())
 	 
 	 }
  }
  
  func proxy(port string) {
-	 addrPort := ":" + port
+     fmt.Println("will start listening")
+     addrPort := ":" + port
 	 ln, err := net.Listen("tcp", addrPort)
 	 if err != nil {
-		 // handle error
+        return
+     }
+     fmt.Println("listening")
  
-	 }
- 
+          
+     fmt.Println("before handling...")
 	 for {
 		 conn, err := ln.Accept()
 		 if err != nil {
 			 // handle error
+             return
  
 		 }
+         fmt.Println("handling...")
 		 go handleConnection(conn)
 	 }
  
